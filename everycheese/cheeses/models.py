842e818e62f1ea479218f020fbd5183c1638b4b2
yeahokyok <982466+yeahokyok@users.noreply.github.com>
2023-02-22 11:58:12 +0700

    add country

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
+from django_countries.fields import CountryField
 
 
 class Cheese(TimeStampedModel):
@@ -23,6 +24,7 @@ class Cheese(TimeStampedModel):
         choices=Firmness.choices,
         default=Firmness.UNSPECIFIED,
     )
+    country_of_origin = CountryField("Country of Origin", blank=True)
 
     def __str__(self):
         return self.name
